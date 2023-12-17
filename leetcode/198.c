d7ff9e42f308e9fc79f4caf5c97a4a0e050dcb6b
linyu13 <3229515040@qq.com>
2023-12-17 20:09:33 +0800

    leetcode

--- leetcode/198.c
+++ leetcode/198.c
@@ -1,30 +1,32 @@
-#include<stdio.h>
-int rob(int* nums, int numsSize) {
-    if(numsSize==0){
+#include <stdio.h>
+int rob(int *nums, int numsSize) {
+    if (numsSize == 0) {
         return 0;
     }
-    if(numsSize==1){
+    if (numsSize == 1) {
         return nums[0];
     }
-    int n=numsSize;
-    int dp[200]={0};
-    dp[0]=nums[0];
-    if(nums[0]>nums[1]){
-        dp[1]=nums[0];//记录前两个比较大的金额
-    }else{
-        dp[1]=nums[1];
+    int n = numsSize;
+    int dp[200] = {0};
+    dp[0] = nums[0];
+    if (nums[0] > nums[1]) {
+        dp[1] = nums[0]; // 记录前两个比较大的金额
+    } else {
+        dp[1] = nums[1];
     }
-    for(int i=2;i<n;i++){
-        if(dp[i-2]+nums[i]>dp[i-1]){//判断该点位的金钱和前两个的金钱和前两个地方所能得到的金钱
-            dp[i]=dp[i-2]+nums[i];
-        }else{
-            dp[i]=dp[i-1];
+    for (int i = 2; i < n; i++) {
+        if (dp[i - 2] + nums[i] >
+            dp[i -
+               1]) { // 判断该点位的金钱和前两个的金钱和前两个地方所能得到的金钱
+            dp[i] = dp[i - 2] + nums[i];
+        } else {
+            dp[i] = dp[i - 1];
         }
     }
-    return dp[n-1];//动态规划！！！！！！！！！！！！！！！！！！！！！！！！！
+    return dp[n -
+              1]; // 动态规划！！！！！！！！！！！！！！！！！！！！！！！！！
 }
-int main()
-{
-    int nums[]={1,2,3,1};
-    rob(nums,4);
+int main() {
+    int nums[] = {1, 2, 3, 1};
+    rob(nums, 4);
 }
--- leetcode/611.c
+++ leetcode/611.c
@@ -1,65 +1,33 @@
 #include <stdio.h>
-void QuickSort(int arr[], int start, int end)// 快速排序(递归)
-{
-	if (start >= end)
-		return;
-	int i = start;
-	int j = end;
-	// 基准数
-	int baseval = arr[start];
-	while (i < j)
-	{
-		// 从右向左找比基准数小的数
-		while (i < j && arr[j] >= baseval)
-		{
-			j--;
-		}
-		if (i < j)
-		{
-			arr[i] = arr[j];
-			i++;
-		}
-		// 从左向右找比基准数大的数
-		while (i < j && arr[i] < baseval)
-		{
-			i++;
-		}
-		if (i < j)
-		{
-			arr[j] = arr[i];
-			j--;
-		}
-	}
-	// 把基准数放到i的位置
-	arr[i] = baseval;
-	QuickSort(arr, start, i - 1);//再次递归该基准数下标之前的数据
-	QuickSort(arr, i + 1, end);//再次递归该基准数下标之后的数据
-}
-
+#include <stdlib.h>
+/*整体思想：
+先对数组进行排序
+排序之后定义两个连续的下标指针
+对后续的所有数组内容进行比较
+使用二分法查找能够组成有效三角形的极限下标
+计算有效的个数并更新之前的连续下标的位置
+*/
+int cmp(int *a, int *b) { return *a - *b; }
 int triangleNumber(int *nums, int numsSize) {
-    int i = 0, j = 0, k = 0;
+    qsort(nums, numsSize, sizeof(int), cmp);
     int cnt = 0;
-    // for (i = 0; i <= numsSize - 3; i++) {
-    //     for (j = i + 1; j <= numsSize - 2; j++) {
-    //         for (k = j + 1; k <= numsSize - 1; k++) {
-    //             if ((nums[i] + nums[j] > nums[k]) &&
-    //                 (nums[i] + nums[k] > nums[j]) &&
-    //                 (nums[k] + nums[j] > nums[i])) {
-    //                 cnt++;
-    //             }
-    //         }
-    //     }
-    // }
-    // 时间复杂度未免有些过高
-    // 使用排序的思想，双指针指向两个相邻的数，每当遇到大于等于这两数之和的数就更新两数的下标
-    QuickSort(nums,0,numsSize);
-    for(i=0;i<=numsSize-3;i++){
-        for(j=i+1;j<=numsSize-2;j++){
-            for(k=j+1;nums[i]+nums[j]<nums[k];k++){
-                    cnt++;
-            }goto re;
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            int left = j + 1;
+            int right = numsSize - 1;
+            int k = j;
+            while (left <= right) {
+                int mid = (left + right) /
+                          2; // 二分实现快速查找排序后的能实现三角形的个数
+                if (nums[mid] < nums[i] + nums[j]) {
+                    k = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+            cnt += k - j;
         }
-        re:
     }
     return cnt;
 }
