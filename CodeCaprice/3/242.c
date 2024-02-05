1081ca479e2fd1c67f71482836d9dfba46fc91f2
linyu13 <3229515040@qq.com>
2024-02-05 21:56:03 +0800

    3-242

---
+++ CodeCaprice/3/242.c
@@ -0,0 +1,24 @@
+#include <stdbool.h>
+#include <stdio.h>
+bool isAnagram(char *s, char *t) {
+    char hash[100] = {0};
+    for (int i = 0; s[i] != '\0'; i++) {
+        hash[i] = s[i];
+    }
+    for (int j = 0; t[j] != '\0'; j++) {
+        for (int k = 0; hash[k] != '\0'; k++) {
+            if (t[j] == hash[k]) {
+                hash[k] = 0;
+                break;
+            }
+            if (t[j] != hash[k] && hash[k + 1] == '\0') {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int main() {
+    int i=isAnagram("anagram","nagaram");
+    printf("%d\n",i);
+}
