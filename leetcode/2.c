0ac254704b798b87ddbd0f27d1f4f56a34e0c35b
linyu13 <3229515040@qq.com>
2024-02-10 23:46:06 +0800

    leetcode

--- leetcode/2.c
+++ leetcode/2.c
@@ -21,10 +21,8 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
         le_l2++;
         head_l2 = head_l2->next;
     }
-
     head_l1 = l1;
     head_l2 = l2;
-
     while (head_l1 != NULL && head_l2 != NULL) {
         if (le_l1 >= le_l2) {
             sum = head_l1->val + head_l2->val + cnt;
@@ -44,7 +42,6 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
         head_l1 = head_l1->next;
         head_l2 = head_l2->next;
     }
-
     while (head_l1 != NULL) {
         sum = head_l1->val + cnt;
         head_l1->val = (head_l1->val + cnt) % 10;
@@ -56,7 +53,6 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
         pre_l1 = head_l1;
         head_l1 = head_l1->next;
     }
-
     while (head_l2 != NULL) {
         sum = head_l2->val + cnt;
         head_l2->val = (head_l2->val + cnt) % 10;
@@ -68,7 +64,6 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
         pre_l2 = head_l2;
         head_l2 = head_l2->next;
     }
-
     if (cnt == 1 && sum >= 10) {
         struct ListNode *new =
             (struct ListNode *)malloc(sizeof(struct ListNode));
@@ -80,13 +75,11 @@ struct ListNode *addTwoNumbers(struct ListNode *l1, struct ListNode *l2) {
             pre_l2->next = new;
         }
     }
-
     if (le_l1 >= le_l2) {
         return l1;
     } else {
         return l2;
     }
-
     return NULL;
 }
 int main() {
